cd2a2c425da3c7ffd025f770f1a6f050734b1fa3
DraxusTDEV <draxustienda@gmail.com>
2022-10-19 22:00:48 -0500

    Agregando estilos y botones para el contador

--- practica/src/App.vue
+++ practica/src/App.vue
@@ -1,47 +1,53 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  const name = 'Práctica VUEJS3'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{name}}</h1>
+  <div class="conta">0</div>
+  <div class="container">
+    <button>Aumentar</button>
+    <button>Disminuir</button>
+    <button>Reset</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style >
+  h1{
+    background: black;
+    color: white;
+    text-align: center;
+    border-radius: 10px;
+    text-shadow: 3px 3px 4px yellow;
+  }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+  .container{
+    width: 100%;
+    height: 100px;
+    background: gray;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: center;
+    align-items: center;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  button{
+    display: inline-block;
+    height: 50px;
+    width: 100px;
+    border-radius: 5px;
+    border: none;
+    margin-right: 10px;
   }
 
-  header .wrapper {
+  .conta{
+    width: 20%;
+    margin: 0 auto;
+    height: 40px;
+    background: linear-gradient(rgb(218, 165, 32), rgb(44, 218, 32));
     display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    font-size: 30px;
+    font-weight: bold;
   }
-}
 </style>
