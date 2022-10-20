26d1a63375c47e95313b716a49a11db45c5d1cf0
DraxusTDEV <draxustienda@gmail.com>
2022-10-19 20:14:22 -0500

    agregué botones disminuir, reset y clases positive y negative

--- reactividad/src/App.vue
+++ reactividad/src/App.vue
@@ -11,16 +11,36 @@ const increment = () => {
   console.log(counter.value);
 }
 
+const decrement = () => {
+  //console.log('aumentar contador');
+  counter.value--; //debemos agregarle el .value
+  //console.log(counter.value);
+}
+
+const reset = () => {
+  //console.log('aumentar contador');
+  counter.value = 0; //debemos agregarle el .value
+  //console.log(counter.value);
+}
+
 </script>
 
 <template>
   <h1>Hola {{name}}</h1>
-  <h2>{{counter}}</h2>
+  <h2 :class="counter > 0 ? 'positive' : 'negative'">{{counter}}</h2>
 
   <button @click="increment">Aumentar</button>
+  <button @click="decrement">Disminuir</button>
+  <button @click="reset">Reset</button>
 
 </template>
 
 <style>
+  .positive{
+    color: greenyellow;
+  }
 
+  .negative{
+    color: red;
+  }
 </style>
