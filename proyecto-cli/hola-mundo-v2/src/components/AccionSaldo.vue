48812e9be8ac99ca9849cd2b929221ac4471aed2
DraxusTDEV <draxustienda@gmail.com>
2022-11-07 10:14:42 -0500

    modificado proyecto cli

---
+++ proyecto-cli/hola-mundo-v2/src/components/AccionSaldo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button @click="accion">{{texto}}</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AccionSaldo',
+        props: {
+            texto: String
+        },
+        methods: {
+            accion(){
+                this.$emit('accion')
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ proyecto-cli/hola-mundo-v2/src/components/Cuenta.vue
@@ -0,0 +1,54 @@
+<template>
+  <h2>Tipo de cuenta: {{cuenta}}</h2>  
+  <h2>Saldo: {{saldo}} </h2>
+  <h2>Cuenta: {{estado ? 'Activa' : 'Inactiva'}}</h2>
+  <h3  v-for="(servicio, index) in servicios" :key="index">
+    {{ index + 1 }} - {{ servicio }}
+  </h3>
+
+  <AccionSaldo @accion="aumentar" texto='Aumentar Saldo'/>
+  <AccionSaldo @accion="disminuir" texto='Disminuir saldo'/>
+
+</template>
+
+
+<script>
+import AccionSaldo from './AccionSaldo'
+
+export default {
+    components: {
+      AccionSaldo,
+    },
+
+    data() {
+        return {
+            saldo: 1000,
+            cuenta: 'Visa',
+            estado: false,
+            servicios: [
+                'Giros',
+                'Abonos',
+                'Transferencias'
+            ]
+        }
+    },
+
+    methods: {
+      aumentar() {
+        this.saldo = this.saldo + 100
+      },
+      disminuir() {
+        if(this.saldo === 0){
+          alert('sin saldo')
+          return
+        }
+        this.saldo = this.saldo - 100
+      }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
