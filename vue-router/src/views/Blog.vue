bba4653013dc0173520360f6f5b079f6325b0307
DraxusTDEV <draxustienda@gmail.com>
2022-11-13 16:43:57 -0500

    vue router project finished

--- vue-router/src/views/Blog.vue
+++ vue-router/src/views/Blog.vue
@@ -1,5 +1,11 @@
 <template>
     <Titulo texto="Título de mi blog" />
+    <!--<button @click="consumirApi">Consumir Api</button>-->
+   <div v-for="(item, index) in arrayBlog" :key="arrayBlog.id">
+        <router-link :to="`/blog/${item.id}`">
+            {{item.title}}
+        </router-link>
+    </div>
 </template>
 
 <script>
@@ -7,7 +13,30 @@ import Titulo from '../components/Titulo.vue'
 export default {
     components: {
         Titulo
-    }
+    },
+
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+
+    methods: {
+        async consumirApi() {
+            try{
+                const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+                const array = await data.json()
+                console.log(array);
+                this.arrayBlog = array
+            }catch(error){
+                console.log(error);
+            }
+        }
+    },
+
+    created () {
+        this.consumirApi();
+    },
 }
 </script>
 
